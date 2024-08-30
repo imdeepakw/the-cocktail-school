2bbd62acd0d27f3d22573fd8c32454486e6cd738
Deepak Wadhwani <66224613+imdeepakw@users.noreply.github.com.>
2024-08-30 12:26:34 -0500

    Added event listener for get drink button

---
+++ script.js
@@ -0,0 +1,12 @@
+//Make a cocktail automatic carousel using the cocktailDB API.
+//Parts of the app: 
+//Getting the user inputted drink from the form input
+//Use the drink to fectch the data from the cocktailDB API.
+//Show the user the first drink from the data
+//For the carrousel, let user click a button to show the next drink on the list of drinks gotten back from the data, also show the total number of drinks by that name and the current drink number.
+//Extra: 
+//1- Allow the user to go back to a previous drink by clicking a button
+//2- Show the ingredients used for the drink to the user 
+//3- Also the measurement of the ingredients used
+
+document.querySelector('.getDrink').addEventListener('click', getDrink)
