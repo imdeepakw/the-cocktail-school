0f726db9a76664b374ff3b56a28ab4ba38ac2d71
Deepak Wadhwani <66224613+imdeepakw@users.noreply.github.com.>
2024-08-31 17:18:02 -0500

    Added type attribute to remove button default submit behaviour. Added the fetch function to retrevie data from the API.

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ document.querySelector('.getDrink').addEventListener('click', getDrink)
 
 function getDrink(){
     let drink = document.querySelector('input').value
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }
